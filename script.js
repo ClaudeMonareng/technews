6c04e3e9df4877481524f9f3737e2db6596e9464
Lehlohonolo Monareng <lehlohonolomonareng@yahoo.com>
2024-03-02 17:06:52 +0200

    Added my project

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const btnHam= document.
+querySelector('.ham-btn');
+const btnTimes= document.
+querySelector('.times-btn');
+const navBar= document.
+getElementById('nav-bar');
+
+btnHam.addEventListener
+('click', function(){
+    if(btnHam.className !== "")
+    {
+        btnHam.style.display
+        = "none";
+        btnTimes.style.display
+        = "block";
+        navBar.classList.add
+        ("nav-bar");
+    }
+})
+
+btnTimes.addEventListener
+('click', function(){
+    if(btnHam.className !== "")
+    {
+        this.style.display
+        = "none";
+        btnHam.style.display 
+        = "block";
+        navBar.classList.remove
+        ("nav-bar");
+    }
+})
